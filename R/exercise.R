4f13a3dd0dc7cb9a1cb9974265bfd1c4695217ed
Moritz Ulrich <moritz@tarn-vedra.de>
2020-07-19 13:32:09 +0200

    Implement `readCorpus` and `removeStopwords`. Tests are failing with minor differences.

--- R/exercise.R
+++ R/exercise.R
@@ -1,1 +1,27 @@
 sw <- c("i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now")
+
+library("datasets")
+
+b <- mean(mtcars$cyl)
+
+readCorpus <- function(dirname = "data/") {
+  vecs <- list()
+
+  for(file in list.files(dirname, pattern="txt$", full.names = TRUE)) {
+    lines <- scan(file, what="char", sep="\n", strip.white=TRUE)
+    vecs <- append(vecs, list(unlist(strsplit(lines, " "))))
+  }
+
+  vecs
+}
+
+stopwords <- c("i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now")
+
+removeStopwords <- function(vecs) {
+  without_stopwords <- list()
+  for(v in vecs) {
+    # Strip stopwords with %in% (https://discuss.analyticsvidhya.com/t/how-to-remove-value-from-a-vector-in-r/2975)
+    without_stopwords <- append(without_stopwords, list(v[!tolower(v) %in% stopwords]))
+  }
+  without_stopwords
+}
